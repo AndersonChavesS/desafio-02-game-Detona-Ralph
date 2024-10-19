079aaaad8286fc82025f722cd0fc0292e9e98f96
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-10-18 21:18:15 -0300

    feat: importado audios do jogo e implementado funções para execução dos audios.

--- src/JS/engine.js
+++ src/JS/engine.js
@@ -4,23 +4,34 @@ const state = {
     enemy: document.querySelector('.enemy'),
     timeLeft: document.querySelector('#time-left'),
     score: document.querySelector('#score'),
+    livesImg: document.querySelector('#lives-img'),
     lives: document.querySelector('#lives'),
     play: document.querySelector('#play'),
     stop: document.querySelector('#stop'),
     pause: document.querySelector('#pause'),
   },
   values: {
-    lives: 3,
-    gameVelocity: 800,
-    hitPosition: null,
-    result: 0,
     currentTime: 60,
+    result: 0,
+    lives: 5,
+    gameVelocity: 1000,
+    hitPosition: null,
+    randonSquareId: null,
   },
   actions: {
     timerId: 0,
     countDownId: 0,
   },
 };
+
+const sounds = {
+  gameOver: new Audio('./src/assets/audios/game-over.mp3'),
+  musicGame: new Audio('./src/assets/audios/audio-game.mp3'),
+};
+
+Object.values(sounds).forEach((sound) => {
+  sound.volume = 0.2;
+});
 
 function playGame() {
   initialize();
@@ -29,47 +40,54 @@ function playGame() {
 function stopGame() {
   clearInterval(state.values.timerId);
   clearInterval(state.actions.countDownId);
-  alert('GAME OVER! Your final score is ' + state.values.result);
-  state.values.lives = 3;
-  state.values.result = 0;
-  state.values.currentTime = 0;
-  state.view.lives.textContent = state.values.lives;
-  state.view.score.textContent = state.values.result;
-  state.view.timeLeft.textContent = state.values.currentTime;
+  playMusic('gameOver');
+  alert(`Game over! Your score is ${state.values.result}`);
   state.view.squares.forEach((square) => {
     square.classList.remove('enemy');
   });
-
+  state.view.play.removeEventListener('click', playGame);
+  state.view.play.addEventListener('click', (e) => {
+    e.preventDefault();
+    location.reload();
+  });
+  state.view.play.textContent = 'Play again';
+  state.view.play.style.display = 'block';
+  state.view.stop.style.display = 'none';
+  state.view.pause.style.display = 'none';
+  inactiveCursorPointer();
 }
 
 function pauseGame() {
-  if (state.values.timerId) {
-    clearInterval(state.values.timerId);
-  }
-  if (state.actions.countDownId) {
-    clearInterval(state.actions.countDownId);
-  }
+  clearInterval(state.values.timerId);
+  clearInterval(state.actions.countDownId);
+  state.view.play.style.display = 'block';
+  state.view.stop.style.display = 'block';
+  state.view.pause.style.display = 'none';
+  inactiveCursorPointer();
 }
 
 function valuesLives() {
-  if (state.values.lives > 0) {
-    state.values.lives--;
-    state.view.lives.textContent = state.values.lives;
-
-    if (state.values.lives === 0) {
-     stopGame();
-    }
-  }
+  state.view.squares.forEach((square) => {
+    square.addEventListener('mousedown', () => {
+      if (square.id !== state.values.hitPosition) {
+        state.values.lives--;
+        state.view.lives.textContent = state.values.lives;
+        playAudio('err');
+      }
+      if (state.values.lives === 0) {
+        stopGame();
+      }
+    });
+  });
 }
 
 function countDown() {
   state.values.currentTime--;
   state.view.timeLeft.textContent = state.values.currentTime;
 
-  if (state.values.currentTime === 0 || state.values.lives === 0) {
-    clearInterval(state.values.timerId);
-    clearInterval(state.actions.countDownId);
-    alert('GAME OVER! Your final score is ' + state.values.result);
+  if (state.values.currentTime === 0 || state.values.currentTime === -1) {
+    stopGame();
+    return;
   }
 }
 
@@ -94,18 +112,60 @@ function addListenerHitBox() {
         state.values.result++;
         state.view.score.textContent = state.values.result;
         state.values.hitPosition = null;
+        playAudio('hit');
       }
     });
   });
 }
 
 function initialize() {
-  let timerId = state.actions.timerId;
-  let countDownId = state.actions.countDownId;
-
-  timerId = setInterval(randomSquare, 1000);
-  countDownId = setInterval(countDown, 1000);
-
+  state.actions.countDownId = setInterval(countDown, 1000);
+  playMusic('musicGame');
   moveEnemy();
+  valuesLives();
+  activeCursorPointer();
   addListenerHitBox();
+
+  state.view.play.style.display = 'none';
+  state.view.stop.style.display = 'block';
+  state.view.pause.style.display = 'block';
+  state.view.livesImg.style.display = 'block';
+  state.view.lives.style.display = 'block';
+  state.view.lives.textContent = state.values.lives;
+  state.view.score.textContent = state.values.result;
+  state.view.timeLeft.textContent = state.values.currentTime;
+}
+
+function activeCursorPointer() {
+  state.view.squares.forEach((square) => {
+    square.style.cursor = 'pointer';
+  });
+}
+
+function inactiveCursorPointer() {
+  state.view.squares.forEach((square) => {
+    square.style.cursor = 'default';
+  });
+}
+
+function playAudio(sound) {
+  let audio = new Audio(`./src/assets/audios/${sound}.mp3`);
+  audio.volume = 0.2;
+  audio.play();
+}
+
+function playMusic(soundName) {
+  stopMusic();
+  if (sounds[soundName]) {
+    sounds[soundName].play();
+  } else {
+    console.error(`Sound ${soundName} not found`);
+  }
+}
+
+function stopMusic() {
+  Object.values(sounds).forEach((sound) => {
+    sound.pause();
+    sound.currentTime = 0;
+  });
 }
